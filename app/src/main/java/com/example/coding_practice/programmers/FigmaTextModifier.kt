4426bbc62144f88dd827b0b9900b06ba2592975f
Lim Myeong Hoon <dla3946gns@naver.com>
2022-04-04 23:14:54 +0900

    Compose UI 라이브러리 종송성 추가

---
+++ app/src/main/java/com/example/coding_practice/programmers/FigmaTextModifier.kt
@@ -0,0 +1,19 @@
+package com.example.coding_practice.programmers
+
+import androidx.compose.ui.layout.LayoutModifier
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.TextUnit
+
+class FigmaTextModifier : LayoutModifier {
+        override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints
+    ): MeasureResult {
+        TODO("Not yet implemented")
+    }
+
+}
